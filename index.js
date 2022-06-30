479a2057299f632ac5621a0d069154d98e294f75
Blood <106900166+Superbigkids@users.noreply.github.com>
2022-07-01 08:44:07 +1200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const clockE1 = document.querySelector('.clock');
+const buttons = document.querySelectorAll('.format button')
+let format ;
+
+setInterval(generateTime, 1000);
+
+function generateTime(){
+    format = clockE1.getAttribute('data-format')
+    const date = new Date;
+    let hour = date.getHours();
+    const min = date.getMinutes();
+    const sec = date.getSeconds();
+    const timeStatus = (hour > 12) ? 'PM' : 'AM';
+    
+    if(format === '12'){
+        hour = (hour > 12) ? hour % 12 : hour;
+    }
+
+    clockE1.innerHTML = `<h1>${hour} : ${min} : ${sec} ${timeStatus}</h1>`
+}
+
+buttons.forEach(button=>{
+    button.addEventListener('click',()=>{
+      format = button.getAttribute('data-format')
+      clockE1.setAttribute('data-format', format)
+      generateTime();
+   })
+})
